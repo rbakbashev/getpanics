e2f80e429a2154484deeff71c7c7620cd6b82b8c
Ruslan Akbashev <ruslashev@gmail.com>
2024-04-14 02:36:56 +0400

    db: get list of targets

--- src/db.rs
+++ src/db.rs
@@ -3,29 +3,60 @@ use std::path::PathBuf;
 
 use ra_ap_ide::RootDatabase;
 use ra_ap_load_cargo as rl;
-use ra_ap_paths::AbsPathBuf;
+use ra_ap_paths::{AbsPathBuf, RelPath, Utf8Path};
 use ra_ap_project_model as ra;
 
 use crate::utils::MaybeError;
 
-pub fn construct(dir: PathBuf) -> RootDatabase {
+pub fn construct(dir: PathBuf) -> (Vec<ra::TargetData>, RootDatabase) {
     let path = AbsPathBuf::assert_utf8(dir);
     let manifest = ra::ProjectManifest::discover_single(&path).or_die("discover project manifest");
     let cargo_config = construct_cargo_config();
     let workspace =
         ra::ProjectWorkspace::load(manifest, &cargo_config, &progress_cb).or_die("load project");
+    let targets = get_targets(&workspace);
     let extra_env = HashMap::default();
     let load_config = construct_load_config();
-    let (db, _vfs, _proc_macro) =
+    let (db, _vfs, _proc_macro_server) =
         rl::load_workspace(workspace, &extra_env, &load_config).or_die("load workspace");
 
-    db
+    (targets, db)
 }
 
 fn construct_cargo_config() -> ra::CargoConfig {
     ra::CargoConfig {
         sysroot: Some(ra::RustLibSource::Discover),
         ..Default::default()
+    }
+}
+
+fn get_targets(workspace: &ra::ProjectWorkspace) -> Vec<ra::TargetData> {
+    match workspace {
+        ra::ProjectWorkspace::Cargo { cargo, .. } => {
+            let mut targets = vec![];
+
+            for package_idx in cargo.packages() {
+                let package = &cargo[package_idx];
+
+                for target_idx in &package.targets {
+                    targets.push(cargo[*target_idx].clone());
+                }
+            }
+
+            targets
+        }
+        ra::ProjectWorkspace::Json { project, .. } => {
+            let _root_path = project.path();
+            todo!()
+        }
+        ra::ProjectWorkspace::DetachedFiles { files, .. } => {
+            let lib_suffix = RelPath::new_unchecked(Utf8Path::new("lib.rs"));
+            let bin_suffix = RelPath::new_unchecked(Utf8Path::new("main.rs"));
+            let _root_paths = files
+                .iter()
+                .filter(|p| p.ends_with(lib_suffix) || p.ends_with(bin_suffix));
+            todo!()
+        }
     }
 }
 
@@ -38,6 +69,4 @@ fn construct_load_config() -> rl::LoadCargoConfig {
 }
 
 #[allow(clippy::needless_pass_by_value)] // it's a callback
-fn progress_cb(_msg: String) {
-    // println!("loading project: {msg}");
-}
+fn progress_cb(_msg: String) {}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 #![allow(
-    clippy::single_match_else,
+    clippy::missing_const_for_fn,
     clippy::option_if_let_else,
-    clippy::missing_const_for_fn
+    clippy::single_match_else,
+    clippy::uninlined_format_args
 )]
 
 mod args;
@@ -10,5 +11,5 @@ mod utils;
 
 fn main() {
     let args = args::parse();
-    let _db = db::construct(args.directory);
+    let (targets, db) = db::construct(args.directory);
 }
